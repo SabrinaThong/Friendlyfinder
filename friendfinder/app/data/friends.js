1f83d4a69bde74214588aa0d2c40fa3e26840e32
Sabrina Thong <sabrinaanhthong@gmail.com>
2019-05-31 23:32:58 -0500

    adding changes to repo

--- friendfinder/app/data/friends.js
+++ friendfinder/app/data/friends.js
@@ -5,7 +5,7 @@ var friends = [{
     },
     {
     "name" : "Sam",
-    "photo":  "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":  "https://thumbs-prod.si-cdn.com/qXrJJ-l_jMrQbARjnToD0fi-Tsg=/800x600/filters:no_upscale()/https://public-media.si-cdn.com/filer/d6/93/d6939718-4e41-44a8-a8f3-d13648d2bcd0/c3npbx.jpg",
     "scores": ["5","1","5","5","1","3","4","2","4","4"]
     },
     {
