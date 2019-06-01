13fb2d531639f8c3cbf1cfe5ec1e11cc186f4ca6
Sabrina Thong <sabrinaanhthong@gmail.com>
2019-06-01 12:54:57 -0500

    adding change to heroku

--- friendfinder/server.js
+++ friendfinder/server.js
@@ -11,7 +11,8 @@ app.use(express.json());
 
 
 require("./app/routing/apiRoutes")(app);
-
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
 
 
 app.listen(PORT, function() {
