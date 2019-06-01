fb5b7479b37dc3070cb4ef274d55a980737dc1b5
Sabrina Thong <sabrinaanhthong@gmail.com>
2019-06-01 17:37:55 -0500

    adding changes to repo

--- friendfinder/server.js
+++ friendfinder/server.js
@@ -7,12 +7,8 @@ var PORT = process.env.PORT || 3200;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-//possibility of adding content
-
-
 require("./app/routing/apiRoutes")(app);
 require('./app/routing/htmlRoutes')(app);
-
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
